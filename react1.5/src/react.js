4bcc9938961919ad8d4d832831296b92e1306966
zdl <3489966805@qq.com>
2025-05-12 18:32:32 +0800

    feat: 添加react.jsx下的createElemnt事件

---
+++ react1.5/src/react.js
@@ -0,0 +1,32 @@
+import { REACT_ELEMENT } from "./constants";
+
+function createElement(type, config, children) {
+    let ref;
+    let key;
+    if (config) {
+        delete config.__source;
+        delete config.__self;
+        ref = config.ref;
+        delete config.ref;
+        key = config.key;
+        delete config.key;
+    }
+    let props = { ...config };
+    if (arguments.length > 3) {
+        props.children = Array.prototype.slice.call(arguments, 2);
+    }else {
+        props.children = children;
+    }
+    return {
+        $$typeof: REACT_ELEMENT,
+        type,
+        ref,
+        key,
+        props,
+    };
+}
+
+const React = {
+    createElement
+}
+export default React

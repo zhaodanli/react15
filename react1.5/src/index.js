3ec72ad5b161f679ca198d500d33438a4044138a
zdl <3489966805@qq.com>
2025-05-16 20:18:41 +0800

    feat: 实现 createElement, 反向继承 适用于需要深度控制组件行为的场景，例如： 拦截和扩展生命周期方法。 修改组件的渲染输出。 注入额外的状态或逻辑。

--- react1.5/src/index.js
+++ react1.5/src/index.js
@@ -1,14 +1,14 @@
-// import React from './react';
-// import ReactDOM from 'react-dom';
+import React from './react';
+import ReactDOM from './react-dom';
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
 
 // 基础组件
 import ReactBase from './IndeBaseComponent';
 
 // 高阶组件
-import ReactHightComponent from './ReactHightComponent'
+// import ReactHightComponent from './ReactHightComponent'
 // 反向继承
 import ReverseInstansComponent from './ReverseInstansComponent'
 
@@ -17,6 +17,10 @@ import ReverseInstansComponent from './ReverseInstansComponent'
 //   document.getElementById('root')
 // );
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(<ReactHightComponent />);
-root.render(<ReverseInstansComponent title="标题" />);
+// root.render(<ReverseInstansComponent title="标题" />);
+ReactDOM.render(
+  <ReverseInstansComponent />,
+  document.getElementById('root')
+);

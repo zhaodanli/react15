b724d135e7df08ec2444b26320b08142c5f4e7bd
zdl <3489966805@qq.com>
2025-06-11 11:17:37 +0800

    feat: 调整备注

--- react18/src/react-reconciler/src/ReactChildFiber.js
+++ react18/src/react-reconciler/src/ReactChildFiber.js
@@ -5,92 +5,36 @@ import { Placement, ChildDeletion } from "./ReactFiberFlags";
 import { HostText } from "./ReactWorkTags";
 
 /**
- * 这段代码定义了 React Fiber 中的子节点调和逻辑（Reconciliation），
- * 用于将新的子节点（newChild）与当前 Fiber 树中的子节点（currentFirstChild）进行比较，
- * 并生成新的 Fiber 节点树。调和的目标是高效地更新 Fiber 树，尽可能复用已有的节点，减少不必要的创建和销毁。
+ * 这个文件实现了 React Fiber 架构中子节点调和（Reconciliation）的核心算法，
+ * 主要负责新旧子节点的对比、复用、插入、删除和移动，生成新的 Fiber 子链表（成新的 Fiber 节点树），
+    * 新的子节点（newChild）与当前 Fiber 树中的子节点（currentFirstChild）
+ * 并为后续的 commit 阶段提供精确的副作用标记
  * /
-/**
- * 创建一个子节点调和器，用于比较新旧子节点并生成新的 Fiber 节点
- * 1. 调和单个或多个子节点，生成新的 Fiber 节点树。
- * 2. 根据 newChild 的类型（对象、数组、字符串等）调用不同的调和函数。
+
+
+/**。
  * @param {是否跟踪副作用} shouldTrackSideEffects 
  * @returns 
  */
 function createChildReconciler(shouldTrackSideEffects) {
-    /** 根据 newChild 的类型（字符串、数字、对象等）创建对应的 Fiber 节点。
-     * @param {*} returnFiber 
-     * @param {*} newChild 
-     * @returns 
-     */
-    function createChild(returnFiber, newChild) {
-        // 如果 newChild 是字符串或数字，调用 createFiberFromText 创建文本节点。
-        if ((typeof newChild === "string" && newChild !== "") || typeof newChild === "number") {
-            const created = createFiberFromText(`${newChild}`);
-            created.return = returnFiber;
-            return created;
-        }
+    /** >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> 辅助函数 <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< */
 
-        // 如果 newChild 是 React 元素，调用 createFiberFromElement 创建元素节点。
-        if (typeof newChild === "object" && newChild !== null) {
-            switch (newChild.$$typeof) {
-                case REACT_ELEMENT_TYPE: {
-                    const created = createFiberFromElement(newChild);
-                    created.return = returnFiber;
-                    return created;
-                }
-                default:
-                    break;
-            }
-        }
-
-        // 如果 newChild 是其他类型，返回 null。
-        return null;
-    }
-
-    /** 为新创建的 Fiber 节点设置索引（index）和副作用标记（Placement）。
-     * .副作用标记逻辑
-     * @param {*} newFiber 
-     * @param {*} newIndex 
-     */
-    function placeChild(newFiber, lastPlacedIndex, newIndex) {
-        newFiber.index = newIndex;
-
-        if (!shouldTrackSideEffects) {
-            return lastPlacedIndex;
-        }
-
-        const current = newFiber.alternate;
-        if (current !== null) {
-            const oldIndex = current.index;
-            if (oldIndex < lastPlacedIndex) {
-                newFiber.flags |= Placement;
-                return lastPlacedIndex;
-            } else {
-                return oldIndex;
-            }
-        } else {
-            newFiber.flags |= Placement;
-            return lastPlacedIndex;
-        }
-        // 如果 shouldTrackSideEffects 为 true，会将 Placement 标记添加到 newFiber.flags，表示该节点需要插入到 DOM 中。
-        // if (shouldTrackSideEffects) newFiber.flags |= Placement;
-    }
-
-    /** 为新创建的 Fiber 节点设置索引（index）和副作用标记（Placement）。
-     * @param {*} newFiber 
-     * @returns 
-     */
+    /** 副作用标记逻辑 */
+    // reconcileChildFibers[主要函数调用] -> placeSingleChild
     function placeSingleChild(newFiber) {
-        // 说明要添加副作用
-        // shouldTrackSideEffects true：表示当前是“更新阶段”，需要跟踪副作用
+        // 说明要添加副作用 shouldTrackSideEffects true：表示当前是“更新阶段”，需要跟踪副作用
         // alternate 指向上一次渲染的旧 Fiber。 为 null：说明这个 Fiber 是新创建的，不是复用的老节点。
-        // 执行插入操作
         if (shouldTrackSideEffects && newFiber.alternate === null) {
             newFiber.flags |= Placement;
         }
         return newFiber;
     }
 
+    // 复用 fiber
+    // reconcileChildFibers[主要函数调用] -> reconcileChildrenArray -> updateSlot -> updateElement -> useFiber
+    // reconcileChildFibers[主要函数调用] -> reconcileChildrenArray -> updateFromMap -> updateTextNode -> updateElement -> useFiber
+    // reconcileChildFibers[主要函数调用] -> reconcileChildrenArray -> updateFromMap -> updateTextNode -> useFiber
+    // reconcileChildFibers[主要函数调用] -> reconcileSingleElement -> useFiber
     function useFiber(fiber, pendingProps) {
         // 创建一个新的 Fiber 节点，复用现有的 Fiber 节点（fiber），并更新其 props。
         const clone = createWorkInProgress(fiber, pendingProps);
@@ -99,6 +43,25 @@ function createChildReconciler(shouldTrackSideEffects) {
         return clone;
     }
 
+    // 删除所有剩余的子节点。
+    // reconcileSingleElement -> deleteRemainingChildren 
+    // reconcileChildrenArray -> deleteRemainingChildren
+    function deleteRemainingChildren(returnFiber, currentFirstChild) {
+        if (!shouldTrackSideEffects) {
+            return;
+        }
+        let childToDelete = currentFirstChild;
+        while (childToDelete !== null) {
+            deleteChild(returnFiber, childToDelete);
+            childToDelete = childToDelete.sibling;
+        }
+        return null;
+    }
+
+    // reconcileSingleElement -> deleteChild
+    // reconcileSingleElement -> deleteRemainingChildren -> deleteChild
+    // reconcileChildrenArray -> deleteChild
+    // reconcileChildrenArray -> deleteRemainingChildren -> deleteChild
     function deleteChild(returnFiber, childToDelete) {
         // 不需要跟踪副作用，则返回 18之前是放在effectlist中， 现在是存在变量里
         if (!shouldTrackSideEffects) {
@@ -113,18 +76,8 @@ function createChildReconciler(shouldTrackSideEffects) {
         }
     }
 
-    // 删除所有剩余的子节点。
-    function deleteRemainingChildren(returnFiber, currentFirstChild) {
-        if (!shouldTrackSideEffects) {
-            return;
-        }
-        let childToDelete = currentFirstChild;
-        while (childToDelete !== null) {
-            deleteChild(returnFiber, childToDelete);
-            childToDelete = childToDelete.sibling;
-        }
-        return null;
-    }
+
+    /** >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> 单节点调和 <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< */
 
     /** 调和单个 React 元素，生成对应的 Fiber 节点
      * 将新 Fiber 节点的 return 指针指向父节点（returnFiber）。
@@ -141,6 +94,7 @@ function createChildReconciler(shouldTrackSideEffects) {
      * @param {*} newFiber 
      * @returns 
      */
+    // 
     function reconcileSingleElement(returnFiber, currentFirstChild, element) {
         const key = element.key;
         let child = currentFirstChild;
@@ -193,55 +147,19 @@ function createChildReconciler(shouldTrackSideEffects) {
         return created;
     }
 
-    function updateElement(returnFiber, current, element) {
-        const elementType = element.type;
-
-        if (current !== null) {
-            // 如果 oldFiber 存在且类型匹配，复用 oldFiber。
-            // 复用 oldFiber，更新其 props。
-            if (current.type === elementType) {
-                const existing = useFiber(current, element.props);
-                existing.return = returnFiber;
-                return existing;
-            }
-        }
-
-        // 如果 oldFiber 是 null，说明没有老节点，直接创建新节点
-        const created = createFiberFromElement(element);
-        created.return = returnFiber;
-        return created;
-    }
-
-    function updateSlot(returnFiber, oldFiber, newChild) {
-        const key = oldFiber !== null ? oldFiber.key : null;
-        if (typeof newChild === "object" && newChild !== null) {
-            switch (newChild.$$typeof) {
-                case REACT_ELEMENT_TYPE: {
-                    // 如果 newChild 是 React 元素，检查 key 是否匹配。
-                    if (newChild.key === key) {
-                        return updateElement(returnFiber, oldFiber, newChild);
-                    } else {
-                        return null;
-                    }
-                }
-                default:
-                    return null;
-                    break;
-            }
-        }
-    }
+    /** >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> 多节点调和 <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< */
 
     /** 调和子节点数组，生成对应的 Fiber 节点链表。
-     * @param {*} returnFiber 
-     * @param {*} currentFirstChild 
-     * @param {*} newChildren 
-     * @returns 
-     * React 源码的 reconcileChildrenArray 实现比你看到的伪代码更复杂，它 break 后还会有一段“map-based diff”逻辑，专门处理这种“乱序/插入/删除混合”的场景：
-        break 后会把剩下的 oldFiber 按 key/type 建立一个 map
-        然后遍历剩下的 newChildren，尝试在 map 里找可复用的 oldFiber，找不到就新建
-        最后把 map 里没用到的 oldFiber 全部删除
-        你当前看到的代码是简化版，适合“头部对齐”场景。更复杂的场景需要 map diff。
-     */
+         * @param {*} returnFiber 
+         * @param {*} currentFirstChild 
+         * @param {*} newChildren 
+         * @returns 
+         * React 源码的 reconcileChildrenArray 实现比你看到的伪代码更复杂，它 break 后还会有一段“map-based diff”逻辑，专门处理这种“乱序/插入/删除混合”的场景：
+            break 后会把剩下的 oldFiber 按 key/type 建立一个 map
+            然后遍历剩下的 newChildren，尝试在 map 里找可复用的 oldFiber，找不到就新建
+            最后把 map 里没用到的 oldFiber 全部删除
+            你当前看到的代码是简化版，适合“头部对齐”场景。更复杂的场景需要 map diff。
+         */
     function reconcileChildrenArray(returnFiber, currentFirstChild, newChildren) {
         let resultingFirstChild = null; // 新链表的头 用于存储新创建的 Fiber 节点链表的第一个节点 最终会作为函数的返回值。
         let previousNewFiber = null; // 新链表的尾 用于存储当前链表中的最后一个节点，方便将新节点连接到链表中。
@@ -273,7 +191,7 @@ function createChildReconciler(shouldTrackSideEffects) {
             }
             lastPlacedIndex = placeChild(newFiber, lastPlacedIndex, newIdx);
             // 为新 Fiber 设置 index 和副作用标记（如 Placement）。
-            placeChild(newFiber, newIdx);
+            // placeChild(newFiber, newIdx);
             // 把新 Fiber 挂到链表上。
             if (previousNewFiber === null) {
                 resultingFirstChild = newFiber;
@@ -336,7 +254,7 @@ function createChildReconciler(shouldTrackSideEffects) {
                 previousNewFiber = newFiber;
             }
         }
-        
+
         if (shouldTrackSideEffects) {
             existingChildren.forEach((child) => deleteChild(returnFiber, child));
         }
@@ -344,7 +262,65 @@ function createChildReconciler(shouldTrackSideEffects) {
         return resultingFirstChild;
     }
 
+    /** >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> 多节点调和/辅助函数 <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< */
+
+    /** 根据 newChild 的类型（字符串、数字、对象等）创建对应的 Fiber 节点。  */
+    // reconcileChildrenArray -> createChild
+    function createChild(returnFiber, newChild) {
+        // 如果 newChild 是字符串或数字，调用 createFiberFromText 创建文本节点。
+        if ((typeof newChild === "string" && newChild !== "") || typeof newChild === "number") {
+            const created = createFiberFromText(`${newChild}`);
+            created.return = returnFiber;
+            return created;
+        }
+
+        // 如果 newChild 是 React 元素，调用 createFiberFromElement 创建元素节点。
+        if (typeof newChild === "object" && newChild !== null) {
+            switch (newChild.$$typeof) {
+                case REACT_ELEMENT_TYPE: {
+                    const created = createFiberFromElement(newChild);
+                    created.return = returnFiber;
+                    return created;
+                }
+                default:
+                    break;
+            }
+        }
+
+        // 如果 newChild 是其他类型，返回 null。
+        return null;
+    }
+
+    /** 副作用标记逻辑
+     * 为新创建的 Fiber 节点设置索引（index）和副作用标记（Placement）。 用于后续 DOM 插入或移动。
+     */
+    // reconcileChildrenArray -> placeChild
+    function placeChild(newFiber, lastPlacedIndex, newIndex) {
+        newFiber.index = newIndex;
+
+        if (!shouldTrackSideEffects) {
+            return lastPlacedIndex;
+        }
+
+        const current = newFiber.alternate;
+        if (current !== null) {
+            const oldIndex = current.index;
+            if (oldIndex < lastPlacedIndex) {
+                newFiber.flags |= Placement;
+                return lastPlacedIndex;
+            } else {
+                return oldIndex;
+            }
+        } else {
+            newFiber.flags |= Placement;
+            return lastPlacedIndex;
+        }
+        // 如果 shouldTrackSideEffects 为 true，会将 Placement 标记添加到 newFiber.flags，表示该节点需要插入到 DOM 中。
+        // if (shouldTrackSideEffects) newFiber.flags |= Placement;
+    }
+
     // 映射剩下的老节点
+    // reconcileChildrenArray -> mapRemainingChildren
     function mapRemainingChildren(returnFiber, currentFirstChild) {
         const existingChildren = new Map();
         let existingChild = currentFirstChild;
@@ -360,6 +336,7 @@ function createChildReconciler(shouldTrackSideEffects) {
         return existingChildren;
     }
 
+    // reconcileChildrenArray -> updateFromMap
     function updateFromMap(existingChildren, returnFiber, newIdx, newChild) {
         if ((typeof newChild === "string" && newChild !== "") || typeof newChild === "number") {
             // 按照索引找匹配的fiber
@@ -380,6 +357,7 @@ function createChildReconciler(shouldTrackSideEffects) {
         return null;
     }
 
+    // reconcileChildrenArray -> updateFromMap -> updateTextNode
     function updateTextNode(returnFiber, current, textContent) {
         // 不是文本节点
         if (current === null || current.tag !== HostText) {
@@ -393,12 +371,55 @@ function createChildReconciler(shouldTrackSideEffects) {
         }
     }
 
+    // reconcileChildrenArray -> updateSlot
+    function updateSlot(returnFiber, oldFiber, newChild) {
+        const key = oldFiber !== null ? oldFiber.key : null;
+        if (typeof newChild === "object" && newChild !== null) {
+            switch (newChild.$$typeof) {
+                case REACT_ELEMENT_TYPE: {
+                    // 如果 newChild 是 React 元素，检查 key 是否匹配。
+                    if (newChild.key === key) {
+                        return updateElement(returnFiber, oldFiber, newChild);
+                    } else {
+                        return null;
+                    }
+                }
+                default:
+                    return null;
+                    break;
+            }
+        }
+    }
+
+    // reconcileChildrenArray -> updateSlot -> updateElement
+    // reconcileChildrenArray -> updateFromMap -> updateTextNode -> updateElement
+    function updateElement(returnFiber, current, element) {
+        const elementType = element.type;
+
+        if (current !== null) {
+            // 如果 oldFiber 存在且类型匹配，复用 oldFiber。
+            // 复用 oldFiber，更新其 props。
+            if (current.type === elementType) {
+                const existing = useFiber(current, element.props);
+                existing.return = returnFiber;
+                return existing;
+            }
+        }
+
+        // 如果 oldFiber 是 null，说明没有老节点，直接创建新节点
+        const created = createFiberFromElement(element);
+        created.return = returnFiber;
+        return created;
+    }
+
+
     /**  调和单个或多个子节点，生成新的 Fiber 节点树。
      * @param {当前 Fiber 节点（父节点）} returnFiber 
      * @param {当前 Fiber 树中的第一个子节点} currentFirstChild 
      * @param {新的子节点（React 元素、文本、数组等）。} newChild 
      * @returns 
      */
+    // 
     function reconcileChildFibers(returnFiber, currentFirstChild, newChild) {
         // 处理单个 React 元素
         if (typeof newChild === "object" && newChild !== null) {
@@ -422,6 +443,10 @@ function createChildReconciler(shouldTrackSideEffects) {
     }
     return reconcileChildFibers;
 }
+
+
+/** >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> 导出接口 <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< */
+
 /** 用于更新阶段，跟踪副作用（shouldTrackSideEffects = true）。
  * 将 newChild 与 currentFirstChild 进行比较。
  * 尽可能复用已有的 Fiber 节点，减少不必要的创建和销毁。

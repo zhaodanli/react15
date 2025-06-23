9f423054c2a35d8d2e39c6e157f8120f8e390c38
zdl <3489966805@qq.com>
2025-06-23 11:47:07 +0800

    feat: 添加 useBoundDispatch

--- reactRedux/src/components/Counter2.jsx
+++ reactRedux/src/components/Counter2.jsx
@@ -1,14 +1,18 @@
-import { useDispatch, useSelector } from '../react-redux';
+import { useDispatch, useSelector, useBoundDispatch } from '../react-redux';
+import actions from '../store/actions/counter2';
 
 export default function Counter2() {
     let state = useSelector(state => state.counter2);
     let dispatch = useDispatch();
+    let boundActions = useBoundDispatch(actions);
 
     return (
         <div>
             <p>Counter2: {state.number}</p>
-            <button onClick={()=>dispatch({type:'ADD2'})}>+</button>
-            <button onClick={()=>dispatch({type:'MINUS2'})}>-</button>
+            {/* <button onClick={()=>dispatch({type:'ADD2'})}>+</button>
+            <button onClick={()=>dispatch({type:'MINUS2'})}>-</button> */}
+            <button onClick={boundActions.add2}>+</button>
+            <button onClick={boundActions.minus2}>-</button>
         </div>
     )
 }

250cc763011f2d33a28b569b7111a42eeafb7879
zdl <3489966805@qq.com>
2025-06-23 12:34:32 +0800

    feat: demo演示

--- reactRedux/src/store/index.js
+++ reactRedux/src/store/index.js
@@ -2,4 +2,23 @@ import { createStore } from '../redux';
 import reducer from './reducers';
 
 const store = createStore(reducer, { counter1: { number: 0 }, counter2: { number: 0 } });
+
+// 中间件要实现的部分 打印日志
+let dispatch = store.dispatch;
+store.dispatch = function (action) {
+    console.log(store.getState());
+    dispatch(action);
+    console.log(store.getState());
+    return action;
+};
+
+// 实现异步
+store.dispatch = function (action) {
+    setTimeout(() => {
+        dispatch(action);
+    }, 1000);
+    return action;
+};
+
+
 export default store;

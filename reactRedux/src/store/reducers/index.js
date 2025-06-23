be27997db309de82e7082f9b5558dbbb0ac0dd22
zdl <3489966805@qq.com>
2025-06-23 10:48:33 +0800

    feat: 添加备注

--- reactRedux/src/store/reducers/index.js
+++ reactRedux/src/store/reducers/index.js
@@ -2,8 +2,10 @@ import { combineReducers} from '../../redux';
 import counter1 from './counter1';
 import counter2 from './counter2';
 
-let rootReducer = combineReducers({
+let combineReducer = combineReducers({
     counter1,
     counter2
 });
-export default rootReducer;
+
+// 每个reducer 对应一个对象 {counter1: {number:0}, counter2: {number:0}}
+export default combineReducer;

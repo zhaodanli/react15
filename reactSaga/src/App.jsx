8846a7ab923bfa95f32a78fddd3d3d103031e870
zdl <3489966805@qq.com>
2025-06-24 16:13:50 +0800

    feat: 添加 configureStore

--- reactSaga/src/App.jsx
+++ reactSaga/src/App.jsx
@@ -1,5 +1,7 @@
 import { useEffect } from 'react';
-import { createStore } from 'redux';
+// import { createStore } from 'redux';
+// import { configureStore } from '@reduxjs/toolkit';
+import { configureStore } from './toolkit';
 
 const ADD = 'ADD'
 const MINUS = 'MINUS'
@@ -11,6 +13,7 @@ function add() {
 function minus() {
     return { type: MINUS }
 }
+
 const reducer = (state = { number: 0 }, action) => {
     switch (action.type) {
         case ADD:
@@ -22,7 +25,13 @@ const reducer = (state = { number: 0 }, action) => {
     }
 }
 
-let store = createStore(reducer);
+// let store = createStore(reducer);
+const store = configureStore({
+    reducer,
+    // preloadedState: {
+
+    // }
+})
 
 function render() {
     const valueEl = document.getElementById('value');
@@ -36,16 +45,28 @@ export default function App() {
     useEffect(() => {
         render(); // 首次渲染
         const unsubscribe = store.subscribe(render);
+
         const addBtn = document.getElementById('add');
         const minusBtn = document.getElementById('minus');
+        const asyncAddBtn = document.getElementById('async-add');
+
         const handleAdd = () => store.dispatch(add());
         const handleMinus = () => store.dispatch(minus());
+        const handleAsyncAdd = () => store.dispatch((dispatch) => {
+            setTimeout(() => {
+                dispatch(add())
+            }, 1000)
+        });
+
         if (addBtn) addBtn.addEventListener('click', handleAdd);
         if (minusBtn) minusBtn.addEventListener('click', handleMinus);
+        if (asyncAddBtn) asyncAddBtn.addEventListener('click', handleAsyncAdd)
+
         return () => {
             unsubscribe();
             if (addBtn) addBtn.removeEventListener('click', handleAdd);
             if (minusBtn) minusBtn.removeEventListener('click', handleMinus);
+            if (asyncAddBtn) asyncAddBtn.removeEventListener('click', handleMinus);
         };
     }, [])
 
@@ -54,6 +75,7 @@ export default function App() {
             <p id="value">0</p>
             <button id="add">+</button>
             <button id="minus">-</button>
+            <button id="async-add">async-add</button>
         </div>
     )
 }
---
+++ reactSaga/src/toolkit/configureStore.js
@@ -0,0 +1,45 @@
+import { combineReducers, applyMiddleware, createStore, compose } from 'redux';
+import * as thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
+const thunkMiddleware = thunk.default || thunk;
+
+function isPlainObject(value) {
+    if (typeof value !== "object" || value === null)
+        return false;
+    return Object.getPrototypeOf(value) === Object.prototype;
+}
+
+
+function configureStore(options = {}) {
+    let {
+        reducer,
+        // middleware=(getDefaultMiddleware) => getDefaultMiddleware().concat(thunkMiddleware, logger), 
+        middleware = [thunkMiddleware, logger],
+        preloadedState,
+        devTools = true,
+        enhancers = undefined,
+    } = options;
+
+    let rootReducer;
+    if (typeof reducer === "function") {
+        rootReducer = reducer;
+    } else if (isPlainObject(reducer)) {
+        // 如果是随想需要合并
+        rootReducer = combineReducers(reducer);
+    }
+
+    // const enhancer = applyMiddleware(...middleware);
+    // 扩展工具
+    // const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+    // const store = createStore(
+    //     rootReducer,         // 第一个参数：reducer
+    //     preloadedState,      // 第二个参数：初始 state（可选）
+    //     enhancer             // 第三个参数：增强器（可选） 要求是一个函数，类型为：(storeCreator: Function) => Function
+    // );
+    // const store = createStore(rootReducer, preloadedState, composeEnhancers(enhancer));
+    const store = createStore(rootReducer, preloadedState);
+    return store;
+}
+
+export default configureStore;

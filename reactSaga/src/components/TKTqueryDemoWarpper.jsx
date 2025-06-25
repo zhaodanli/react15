6820abba396b355b8a4d57582bcbf7c7e29943da
zdl <3489966805@qq.com>
2025-06-25 21:48:10 +0800

    feat:移动异步demo

---
+++ reactSaga/src/components/TKTqueryDemoWarpper.jsx
@@ -0,0 +1,23 @@
+import { Provider } from 'react-redux';
+import ToolkitQueryDemo from './components/ToolkitQueryDemo';
+
+import { configureStore } from '@reduxjs/toolkit'
+//import { configureStore } from './toolkit'
+import todosApi from './todos'
+
+// API slice会包含自动生成的redux reducer和一个自定义中间件
+const store = configureStore({
+    reducer: {
+        [todosApi.reducerPath]: todosApi.reducer,
+    },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(todosApi.middleware)
+})
+
+export default function App() {
+    return (
+        <Provider store={store}>
+            <ToolkitQueryDemo />
+        </Provider>
+    )
+}

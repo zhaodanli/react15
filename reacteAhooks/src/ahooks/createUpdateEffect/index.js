cba7d7e9076bd26d118cc49901c552f6f8126723
zdl <3489966805@qq.com>
2025-06-26 17:04:29 +0800

    feat: createUpdateEffect 插件

---
+++ reacteAhooks/src/ahooks/createUpdateEffect/index.js
@@ -0,0 +1,17 @@
+import { useRef } from 'react';
+
+export const createUpdateEffect = hook => (effect, deps) => {
+    const isMounted = useRef(false);
+    hook(() => {
+        return () => {
+            isMounted.current = false;
+        };
+    }, []);
+    hook(() => {
+        if (!isMounted.current) {
+            isMounted.current = true;
+        } else {
+            return effect();
+        }
+    }, deps);
+};

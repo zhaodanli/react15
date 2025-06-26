f89cae2ee31d54d0eb1379ba9aedbe03f727a9f8
zdl <3489966805@qq.com>
2025-06-26 17:59:22 +0800

    feat: 添加备注

--- reacteAhooks/src/ahooks/useRequest/src/useRequestImplement.js
+++ reacteAhooks/src/ahooks/useRequest/src/useRequestImplement.js
@@ -3,6 +3,7 @@ import useUpdate from '../../useUpdate';
 import useCreation from '../../useCreation';
 import useMount from '../../useMount';
 import Fetch from './Fetch';
+// 取消请求
 import useUnmount from '../../useUnmount';
 
 // 手动触发 useMemoizedFn是持久化 function 的 Hook，理论上，可以使用 useMemoizedFn 完全代替 useCallback
@@ -22,14 +23,17 @@ function useRequestImplement(service, options = {}, plugins = []) {
     // 因为 useMemo 不能保证被 memo 的值一定不会被重计算，而 useCreation 可以保证这一点
     const fetchInstance = useCreation(() => {
         // return new Fetch(serviceRef, update);
+        // 老的 options 给 onInit 执行，返回 { name } initState = [], 留下返回值不为空数组
         const initState = plugins.map(p => p?.onInit?.(fetchOptions)).filter(Boolean);
+        // 把插件返回的初始状态累加
         return new Fetch(serviceRef, fetchOptions, update, Object.assign({}, ...initState));
     }, []);
-    
+
     //fetchInstance.options = fetchOptions;
+    // 添加插件实现， 每个插件是个函数，接收 实例 和 fetchOptions
     fetchInstance.pluginImpls = plugins.map(p => p(fetchInstance, fetchOptions));
 
-    // useMount是只在组件初始化时执行的 Hook src\ahooks\useMount\index.js
+    // useMount是只在组件初始化时执行的
     useMount(() => {
         if (!manual) {
             const params = fetchInstance.state.params || options.defaultParams || [];
@@ -37,6 +41,7 @@ function useRequestImplement(service, options = {}, plugins = []) {
         }
     });
 
+    // 取消请求
     useUnmount(() => {
         fetchInstance.cancel();
     });
@@ -49,7 +54,8 @@ function useRequestImplement(service, options = {}, plugins = []) {
         refresh: useMemoizedFn(fetchInstance.refresh.bind(fetchInstance)),
         refreshAsync: useMemoizedFn(fetchInstance.refreshAsync.bind(fetchInstance)),
         mutate: useMemoizedFn(fetchInstance.mutate.bind(fetchInstance)),
-        cancel: useMemoizedFn(fetchInstance.cancel.bind(fetchInstance))
+        cancel: useMemoizedFn(fetchInstance.cancel.bind(fetchInstance)),
+        params: fetchInstance.state.params || [],
     };
 }
 

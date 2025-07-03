47f83f05d6160f7d19bdb5adcdcdd2e3877883d6
zdl <3489966805@qq.com>
2025-07-03 22:02:54 +0800

    feat: 支持css文件

--- ReactSSR/src/server/index.js
+++ ReactSSR/src/server/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { renderToString } from 'react-dom/server';
 import { StaticRouter } from "react-router-dom/server";
 import proxy from 'express-http-proxy';
+import StyleContext from 'isomorphic-style-loader-react18/StyleContext'
 import App from '../App';
 import { getServerStore } from '../store';
 import { matchRoutes } from 'react-router-dom';
 import routesConfig from '../routesConfig';
+// import { Helmet } from 'react-helmet';
 
 const express = require('express');
 const app = express();
@@ -18,7 +20,7 @@ app.use('/api', proxy('http://localhost:8080', {
     }
 }));
 
-app.get(/^\/(.+)$/, (req, res) => {
+app.get(/^\/(.*)$/, (req, res) => {
     if (req.url === '/.well-known/appspecific/com.chrome.devtools.json') {
         req.url = '/'
     }
@@ -34,7 +36,7 @@ app.get(/^\/(.+)$/, (req, res) => {
             .filter(Boolean)
 
         Promise.all(promises).then((data) => {
-                
+
             // 服务端坐重定向
             if (req.url === '/profile' && (!(store.getState().auth.user))) {
                 return res.redirect('/login');
@@ -42,28 +44,47 @@ app.get(/^\/(.+)$/, (req, res) => {
                 res.statusCode = 404;
             }
 
+            // 支持css
+            const css = new Set()
+            const insertCss = (...styles) => styles.forEach(style => {
+                css.add(style._getCss())
+            })
+
+            // 支持SEO
+            // let helmet = Helmet.renderStatic();
+
             const html = renderToString(
                 <StaticRouter location={req.url}>
-                    <App store={store} />
+                    <StyleContext.Provider value={{ insertCss }}>
+                        <App store={store} />
+                    </StyleContext.Provider>
                 </StaticRouter>
             );
 
-            // console.log('html', html)
+                        // ${helmet.title.toString()}
+                        // ${helmet.meta.toString()}
+            let style = null;
+
+            console.log('css', css.size)
+            if(css.size > 0) {
+                style = `\n<style>${[...css].join('')}</style>`
+            }
             res.send(`
                 <html>
                     <head>
-                    <meta charset="UTF-8">
-                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-                    <title>ssr</title>
+                        <meta charset="UTF-8">
+                        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+                        <title>ssr</title>
+                        ${style}
                     </head>
                     <body>
                     <div id="root">${html}</div>
-                <script>
+                    <script>
                     var context = {
                         state:${JSON.stringify(store.getState())}
                     }
-                </script>
+                    </script>
                     <script src="/client.js"></script>
                 </body>
                 </html>
